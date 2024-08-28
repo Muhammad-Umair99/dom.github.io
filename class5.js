49cdae1bc6b58aa4dc4369e0f960cf2761ea25d9
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-28 20:26:21 +0500

    dom properties & practice quest

--- class5.js
+++ class5.js
@@ -49,3 +49,57 @@
 
 
 
+    // second file
+    //PROPERTIES
+    //1)tagName
+    // let firstEl = document.querySelector("#my-id");//1st element return
+    // console.dir(firstEl.tagName);
+
+    // let firstEl = document.querySelector("p");//1st element return
+    // console.dir(firstEl.tagName);
+
+
+    // 2) innetext
+    // let div = document.querySelector("div");
+    // console.dir(div);
+    
+    // let di = document.querySelector("div").innerText
+    // console.dir(di);
+
+    //3) innerhtml
+        // let div = document.querySelector("div").innerHTML
+        // console.dir(div);
+
+
+    // 4) textContent
+    //    let heading = document.querySelector("h1")
+    //     console.dir(heading);
+
+
+    // practice question
+    // 1)
+    //create a h2 heading element with text "hellow javascript" append from "js students" to this text using js
+
+    // let h2 = document.querySelector("h2") 
+    // console.dir(h2.innerText);
+    // h2.innerText = h2.innerText + "learn from w3school";
+
+
+    //2) create three div with common class name - "box". acess them & add some unique text to each of them"
+
+    //advance way
+
+    // let box = document.querySelectorAll(".box");
+    // let idex = 1;
+    // for( div of box){
+    //     div.innerText = `new unique value  ${idex}`
+    //     idex++; 
+    // }
+
+
+    //bsic way
+    // box[0].innerText = "new unoque value 1"
+    // box[1].innerText = "new unoque value 2"
+    // box[2].innerText = "new unoque value 3"
+
+

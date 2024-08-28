95ec3c7b11801b13af10c26aac84a3d3128c34a9
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-28 11:32:00 +0500

    dom & dom manipulation

---
+++ class5.js
@@ -0,0 +1,51 @@
+// console.dir(document.body);
+
+// console.log(document.body);
+
+// console.log(document.head);
+
+// console.dir(document.body.childNodes[1].innerText = "acbb");
+
+// document.body.style.background = "green"
+
+
+//DOM MANIPULATION
+//select with id
+// let button = document.getElementById("my-id");
+// console.dir(button);
+
+//select with className (return html collction similar like array)
+// let headings =  document.getElementsByClassName("my-class");
+// console.dir(headings);
+// console.log(headings);
+
+
+// select with tag
+// let paras = document.getElementsByTagName("p");
+// console.dir(paras);
+// console.log(paras);
+
+//QUERY SELECTOR (UPGRADE WAY TO ACESS ELEMENTS)
+//by tag name
+    // let firstEl = document.querySelector("p");//1st element return
+    // console.dir(firstEl);
+
+    // let allEl = document.querySelectorAll("p");//all element return
+    // console.dir(allEl);
+
+
+    //by className
+    //  let firstEl = document.querySelector(".my-class");//1st element return
+    // console.dir(firstEl);
+
+    // let allEl = document.querySelectorAll(".my-class");//all element return
+    // console.dir(allEl); 
+
+    //by id    
+    //  let firstEl = document.querySelector("#my-id");//1st element return
+    // console.dir(firstEl);
+
+
+
+
+
